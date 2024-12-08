52e86c0a700904caea6f3a32ab69f1d92336f418
caroltr <caroltr.srs@gmail.com>
2024-12-08 08:17:49 +0000

    add search top bar

--- app/src/main/java/com/catenri/hogwartshall/MainActivity.kt
+++ app/src/main/java/com/catenri/hogwartshall/MainActivity.kt
@@ -3,7 +3,6 @@ package com.catenri.hogwartshall
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,14 +12,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,18 +46,46 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             HogwartsHallTheme {
                 val mainUiStaStateFlow by viewModel.mainUiStaStateFlow.collectAsStateWithLifecycle()
+                var searchQuery by remember { mutableStateOf("") }
+                var isSearchActive by remember { mutableStateOf(false) }
 
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar  = {
+                        SearchTopBarComponent(
+                            isSearchActive = isSearchActive,
+                            query = searchQuery,
+                            onTabClick = { isSearchActive = true },
+                            onSearchIconClick = {
+                                isSearchActive = false
+                                searchQuery = ""
+                            },
+                            onQueryChange = {
+                                searchQuery = it
+                            }
+                        )
+                    }
+                ) { innerPadding ->
                     when (mainUiStaStateFlow) {
                         MainUiState.Loading -> LoadingComponent()
                         MainUiState.LoadFailed -> LoadFailedComponent()
                         is MainUiState.Success -> {
+                            val characters = (mainUiStaStateFlow as MainUiState.Success).characters
+                            val filteredCharacters = if (searchQuery.isNotEmpty()) {
+                                characters.filter {
+                                    it.name.contains(searchQuery, ignoreCase = true) ||
+                                            it.actor.contains(searchQuery, ignoreCase = true)
+                                }
+                            } else {
+                                characters
+                            }
+
                             CharactersList(
-                                    characters = (mainUiStaStateFlow as MainUiState.Success).characters,
+                                    characters = filteredCharacters,
                                     modifier = Modifier.padding(innerPadding)
                                 )
                         }
@@ -56,6 +94,64 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchTopBarComponent(
+    isSearchActive: Boolean,
+    query: String,
+    onTabClick: () -> Unit,
+    onSearchIconClick: () -> Unit,
+    onQueryChange: (String) -> Unit
+) {
+    if (isSearchActive) {
+        SearchComponent(
+            query,
+            onIconClick = onSearchIconClick,
+            onQueryChange = onQueryChange
+        )
+    } else {
+        TopAppBar(
+            title = { Text("Hogwarts Hall") },
+            actions = {
+                IconButton(onClick = onTabClick) {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = "Search"
+                    )
+                }
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchComponent(
+    query: String,
+    onIconClick: () -> Unit,
+    onQueryChange: (String) -> Unit
+) {
+    SearchBarDefaults.InputField(
+        query = query,
+        onQueryChange = onQueryChange,
+        onSearch = { /* no-op */ },
+        expanded = false,
+        onExpandedChange = { /* no-op */ },
+        placeholder = { Text("Search characters") },
+        leadingIcon = {
+            IconButton(onClick = onIconClick) {
+                Icon(
+                    Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    )
 }
 
 @Composable
@@ -144,7 +240,7 @@ fun CharacterItemPreview() {
                 actor = "Daniel",
                 alive = true,
                 dateOfBirth = null,
-                house = "Gryphyndor",
+                house = "gryffindor",
                 id = "",
                 image = "",
                 name = "Harry",
